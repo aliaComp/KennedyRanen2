61fac5e8957f8d70a1473fd41b3f34f511584426
aliaComp <58254632+aliaComp@users.noreply.github.com>
2021-08-29 21:03:10 +0300

    Add files via upload

---
+++ Task1-5/src/main/java/Kenney.java
@@ -0,0 +1,22 @@
+import com.sun.org.apache.xpath.internal.objects.XString;
+
+public class Kenney {
+        public static void main (String[] args){
+            System.out.println("\"To be");
+            System.out.println("\t\tor not");
+            System.out.println("\t\t\t\tto be...\"");
+            System.out.println("\t\t\t\t\t\t/\tShakespeare\t/");
+
+
+            System.out.println("I love Java!\n\t\tI love Java!\n\t\t\t\tI love Java! \n\t\t\t\t\t\tI love Java! \n\t\t\t\t\t\t\t\tI love Java!");
+
+            System.out.println("\'\\t\',\t\'\\n\', \t\'\\\\', \t\'\\'\', \t\'\\\"\',");
+
+            System.out.println("System.out.println(\"\\'\\\\t\\',\\t\\'\\\\n\\', \\t\\'\\\\\\\\', \\t\\'\\\\'\\', \\t\\'\\\\\\\"\\',\")");
+
+            System.out.println("\t\tСентябрь \nпн\tвт\tср\tчт\tпт\tсб\tвс \n\t\t1 \t2 \t3\t4\t5 \n6\t7\t8\t9\t10\t11\t12 \n12\t14\t15\t16\t17\t18\t19 \n20\t21\t22\t23\t24\t25\t26 \n27\t28\t29\t30");
+
+
+        }
+    }
+
